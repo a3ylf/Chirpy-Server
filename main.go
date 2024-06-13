d4216a8eed95feec25a3973c636cc71dd1e14d58
apns <alexandre3ylf@gmail.com>
2024-06-13 00:30:10 -0300

    pog

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 
 func main() {
     mux := http.NewServeMux()
-    mux.Handle("/assets/logo.png",http.FileServer(http.Dir(".")))
+    mux.Handle("/app/",http.StripPrefix("/app",http.FileServer(http.Dir("."))))
+    mux.HandleFunc("/healthz",handlerReadiness)
+
     server := &http.Server{
 		Addr:    ":8080",  // Define the address and port to listen on
 		Handler: mux,      // Use the new ServeMux as the server's handler
@@ -15,3 +17,8 @@ func main() {
 	log.Printf("Serving on port: 8080\n")
 	log.Fatal(server.ListenAndServe())
 } 
+func handlerReadiness(w http.ResponseWriter, r *http.Request){
+    w.Header().Add("Content-Type","text/plain; charset=utf-8")
+    w.WriteHeader(200)
+    w.Write([]byte("OK"))
+}
